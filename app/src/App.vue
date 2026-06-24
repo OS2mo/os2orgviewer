47c5b4a35c0ca7a3796cba9e7a0d0ae9946e7d35
mb <mb@magenta.dk>
2026-06-24 09:39:18 +0000

    fix: remove unused org_unit_hierarchy_uuid computed with wrong getter name

--- app/src/App.vue
+++ app/src/App.vue
@@ -119,9 +119,6 @@ export default {
     global_root_uuid: function () {
       return this.$store.getters.getGlobalRootUuid
     },
-    org_unit_hierarchy_uuid: function () {
-      return this.$store.getters.getOrgUnitHierarchy
-    },
   },
   methods: {
     checkFirstLoad: function () {
